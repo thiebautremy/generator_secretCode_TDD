ef0f29eef18f36330700987234241885c1727767
thiebautremy <remythiebaut@msn.com>
2022-05-19 22:46:31 +0200

    change input + add style

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./form.css";
 import { useSelector, useDispatch } from "react-redux";
 import {
@@ -6,11 +6,14 @@ import {
   updatePhrase,
   updateJavanisedPhrase,
 } from "../../redux/phrasesSlice";
+import { SwitchTransition, CSSTransition } from "react-transition-group";
 import { javanais_generator } from "../../services/functions";
 
 const Form = () => {
   const phraseToChange = useSelector((state) => state.phrases.phraseToChange);
   const phraseToAdd = useSelector((state) => state.phrases.stringToAdd);
+  const phraseJavanised = useSelector((state) => state.phrases.javanisedPhrase);
+  const [state, setState] = useState(true);
   const dispatch = useDispatch();
   const handleOnSubmit = (e) => {
     e.preventDefault();
@@ -19,43 +22,45 @@ const Form = () => {
     );
   };
   return (
-    <form onSubmit={(e) => handleOnSubmit(e)}>
-      <label htmlFor="phraseToChange">Phrase à Javaniser</label>
-      <input
-        aria-label="Changer la phrase"
-        value={phraseToChange}
-        type="text"
-        name="phraseToChange"
-        onChange={(e) => dispatch(updatePhrase(e.target.value))}
-      />
-      <label htmlFor="textToAdd">Texte à ajouter</label>
-      <input
-        name="textToAdd"
-        aria-label="Changer la phrase à ajouter"
-        value={phraseToAdd}
-        type="text"
-        onChange={(e) => dispatch(updateStringToAdd(e.target.value))}
-      />
-      <button type="submit">Soumettre</button>
+    <main>
+      <form onSubmit={(e) => handleOnSubmit(e)}>
+        <label htmlFor="phraseToChange">Phrase à Javaniser</label>
+        <input
+          aria-label="Changer la phrase"
+          value={phraseToChange}
+          type="text"
+          name="phraseToChange"
+          onChange={(e) => dispatch(updatePhrase(e.target.value))}
+        />
+        <label htmlFor="textToAdd">Texte à ajouter</label>
 
-      <div className="glassmorph">
-        <span>testes</span>
-        <p>
-          Voluptate ullamco sit adipisicing nostrud sit laborum ipsum aliquip.
-          Sunt incididunt pariatur id et ex proident cupidatat est tempor.
-          Veniam laborum enim culpa exercitation aliquip minim nostrud occaecat
-          elit laboris deserunt. Minim cillum culpa non pariatur occaecat. Et
-          anim culpa commodo nisi qui duis ad consequat minim eiusmod. Nostrud
-          cupidatat eiusmod et voluptate minim. Cillum consectetur consectetur
-          id et ex enim sunt laboris esse do mollit. Esse elit est do sit mollit
-          magna est cillum id enim sint exercitation cillum. Magna irure
-          cupidatat qui deserunt ut veniam consectetur excepteur nisi in do
-          officia. Aliquip non in eiusmod culpa aliquip ullamco ullamco magna
-          nostrud fugiat. Magna deserunt consequat nostrud labore reprehenderit
-          consequat consequat sit.{" "}
-        </p>
-      </div>
-    </form>
+        <select
+          name="textToAdd"
+          id="textToAdd"
+          onChange={(e) => dispatch(updateStringToAdd(e.target.value))}
+        >
+          <option value="ja">ja</option>
+          <option value="av">av</option>
+        </select>
+        <button type="submit" onClick={() => setState((state) => !state)}>
+          Soumettre
+        </button>
+      </form>
+
+      <SwitchTransition>
+        <CSSTransition
+          key={state ? "Goodbye, world!" : "Hello, world!"}
+          addEndListener={(node, done) =>
+            node.addEventListener("transitionend", done, false)
+          }
+          classNames="fade"
+        >
+          <div className="glassmorph">
+            {phraseJavanised && <p>{phraseJavanised}</p>}
+          </div>
+        </CSSTransition>
+      </SwitchTransition>
+    </main>
   );
 };
 export default Form;
